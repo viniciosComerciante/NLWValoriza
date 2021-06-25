fba7cb450d518a1c93836144ac8dd5ded0f9ce69
vinicios <vinicompif@gmail.com>
2021-06-25 19:03:04 -0300

    class 04 finished

--- src/routes.ts
+++ src/routes.ts
@@ -3,16 +3,20 @@ import { CreateUserController } from "./controller/CreateUserController";
 import { CreateTagController } from "./controller/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import { CreateComplimentController } from "./controller/CreateComplimentController";
+
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/users", createUserController.handle)
 router.post("/tags", ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
+router.post("/compliments", createComplimentController.handle)
 
 
 export { router };
